d145fb21812dba8624919d91719c7494db024107
lalitote <beata.wasylkiewicz@gmail.com>
2016-07-01 13:38:55 +0200

    FaceIt Demo - end of lecture 4
    Developing iOS Apps with Swift / cs193P.

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by lalitote on 27.06.2016.
+//  Copyright © 2016 lalitote. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
